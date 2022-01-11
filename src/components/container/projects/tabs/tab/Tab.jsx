cf07899f05d197adef6d5ad17c54ea2748b4ef68
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 20:26:31 -0300

    remove unsed href

--- src/components/container/projects/tabs/tab/Tab.jsx
+++ src/components/container/projects/tabs/tab/Tab.jsx
@@ -2,6 +2,6 @@ import styles from './styles.module.scss'
 
 export const Tab = ({ title, activeTab, handleTabClick, index }) => {
   return (
-    <a id='#tab' className={`${styles.tab} ${activeTab === index ? `${styles.active}` : ''}`} href='#foo' onClick={(event) => handleTabClick(index, event)}> {title}</a>
+    <a id='#tab' className={`${styles.tab} ${activeTab === index ? `${styles.active}` : ''}`} onClick={(event) => handleTabClick(index, event)}> {title}</a>
   )
 }
