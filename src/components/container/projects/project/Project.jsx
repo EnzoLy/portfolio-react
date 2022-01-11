b15ec2075192415582b95905c9c3f7bc13c46f26
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 00:33:29 -0300

    change projects contents

--- src/components/container/projects/project/Project.jsx
+++ src/components/container/projects/project/Project.jsx
@@ -1,10 +1,21 @@
 import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
 
 export const Project = ({ title, url, imgUrl, recent }) => {
   return (
-    <a className={`${styles.project}`} href={url} id={recent ? styles.recent : ''}>
-      <img src={imgUrl} alt={`project-img-${title}`} />
-      <h2>{title}</h2>
-    </a>
+    <div className={`${styles.project}`}>
+      <header>
+        <h2>{title}</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+        </p>
+        <div>
+          <a href='#'> Visit </a>
+        </div>
+      </header>
+      <a className={styles.icon} href='https://github.com/enzoly' target='_blank' rel='noreferrer'>
+        <FaGithub />
+      </a>
+    </div>
   )
 }
