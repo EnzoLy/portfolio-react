41b359bc651288794307b68533ae75f08532704e
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 20:24:41 -0300

    added global icons

---
+++ src/components/icons/Icons.jsx
@@ -0,0 +1,41 @@
+import { FaGithub, FaReact } from 'react-icons/fa'
+import { SiNextdotjs, SiVite } from 'react-icons/si'
+
+const deafultIconStyles = {
+  fontSize: '1.6em',
+  marginLeft: '5px',
+  marginRight: '5px',
+  marginTop: '10px'
+}
+
+export const GitHubIcon = ({ href, styles }) => {
+  return (
+    <a href={href} style={{ ...deafultIconStyles, color: '#000', ...styles }} target='_blank' rel='noreferrer'>
+      <FaGithub />
+    </a>
+  )
+}
+
+export const NextJsIcon = ({ styles }) => {
+  return (
+    <a href='https://nextjs.org/' style={{ ...deafultIconStyles, color: '#000', ...styles }} target='_blank' rel='noreferrer'>
+      <SiNextdotjs />
+    </a>
+  )
+}
+
+export const ReactIcon = ({ styles }) => {
+  return (
+    <a href='https://reactjs.org/' style={{ ...deafultIconStyles, color: '#5cd3f3', ...styles }} target='_blank' rel='noreferrer'>
+      <FaReact />
+    </a>
+  )
+}
+
+export const ViteIcon = ({ styles }) => {
+  return (
+    <a href='https://vitejs.dev/' style={{ ...deafultIconStyles, color: '#ffc820', ...styles }} target='_blank' rel='noreferrer'>
+      <SiVite />
+    </a>
+  )
+}
