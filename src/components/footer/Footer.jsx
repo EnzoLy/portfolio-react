d227008eec25c3ed9b037058f6eb4004cb29d1e4
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 20:26:08 -0300

    adding globla icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
 import { FooterItem } from './item/FooterItem'
 import styles from './styles.module.scss'
-import { FaGithub, FaReact } from 'react-icons/fa'
-import { SiVite } from 'react-icons/si'
+import { GitHubIcon, ReactIcon, ViteIcon } from '../icons/Icons'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <FooterItem title='Made with React and Vite' href='https://vitejs.dev/guide/' Icon={[FaReact, SiVite]} />
-      <FooterItem title='Repository' href='https://github.com/EnzoLy' Icon={[FaGithub]} />
+      <FooterItem title='Made with React and Vite' href='https://vitejs.dev/guide/' Icon={[ReactIcon, ViteIcon]} />
+      <FooterItem title='Repository' href='https://github.com/EnzoLy' Icon={[GitHubIcon]} />
     </footer>
   )
 }
