6a22737b269562e4479568d5c5e80d6d399f9a02
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 21:06:09 -0300

    added socials

--- src/components/container/contact/Contact.jsx
+++ src/components/container/contact/Contact.jsx
@@ -1,8 +1,9 @@
+import { TwitterIcon } from '../../icons/Icons'
 import style from './styles.module.scss'
 
 export const Contact = () => {
   return (
-    <section id='#contact' className={style.contact_container}>
+    <section id='contact' className={style.contact_container}>
       <h2>Contact</h2>
       <form className={style.contact}>
         <span>Email </span>
@@ -11,6 +12,10 @@ export const Contact = () => {
         <textarea name='message' placeholder='Message' />
         <button type='submit'>Send</button>
       </form>
+      <h3>Socials</h3>
+      <article className={style.socials}>
+        <TwitterIcon href='https://twitter.com/EnzoL_' styles={{ fontSize: '2.5em' }} />
+      </article>
     </section>
   )
 }
