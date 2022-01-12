39b7de7febb48fcec63dac4abe7550e458c43968
EnzoLy <enzoloyola_@outlook.com>
2022-01-12 01:57:36 -0300

    remove href and improvide text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import { GitHubIcon, ReactIcon, ViteIcon } from '../icons/Icons'
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <FooterItem title='Made with React and Vite' href='https://vitejs.dev/guide/' Icon={[ReactIcon, ViteIcon]} />
+      <FooterItem title='Site made with React and Vite' Icon={[ReactIcon, ViteIcon]} />
       <FooterItem title='Repository' href='https://github.com/EnzoLy' Icon={[GitHubIcon]} />
     </footer>
   )
