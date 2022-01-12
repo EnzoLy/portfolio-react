e2560f823c5c748ecafbc1e5cc2a1b56bce52f62
EnzoLy <enzoloyola_@outlook.com>
2022-01-11 21:59:13 -0300

    added 'teconologies' for icons

--- src/services/getAllProjects.js
+++ src/services/getAllProjects.js
@@ -5,75 +5,87 @@ export const getAllProjects = (tabIndex) => {
       title: 'Project 1',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: true
+      recent: true,
+      teconologies: ['React', 'Vite']
     }, {
       tab: 0,
       title: 'Project 2',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['React', 'Vite']
     }, {
       tab: 0,
       title: 'Project 3',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['NextJs']
     }, {
       tab: 0,
       title: 'Project 4',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['NextJs']
     },
     {
       tab: 1,
       title: 'Project 5',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: true
+      recent: true,
+      teconologies: ['Java']
     }, {
       tab: 1,
       title: 'Project 6',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['Java']
     }, {
       tab: 1,
       title: 'Project 7',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['Kotlin']
     }, {
       tab: 1,
       title: 'Project 8',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['Kotlin']
     },
     {
       tab: 2,
       title: 'Project 9',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: true
+      recent: true,
+      teconologies: ['NodeJs', 'expressjs']
     }, {
       tab: 2,
       title: 'Project 10',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['NodeJs']
     }, {
       tab: 2,
       title: 'Project 11',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['Javascript']
     }, {
       tab: 2,
       title: 'Project 12',
       url: '#',
       imgUrl: 'https://via.placeholder.com/300x200',
-      recent: false
+      recent: false,
+      teconologies: ['Javascript']
     }
   ]
 
