7d42c870f46d0fdc38eff6fc127bddde0c7276a0
EnzoLy <enzoloyola_@outlook.com>
2022-01-12 01:58:44 -0300

    fix icons in mobile

--- src/components/footer/item/FooterItem.jsx
+++ src/components/footer/item/FooterItem.jsx
@@ -4,13 +4,15 @@ export const FooterItem = ({ title, href, Icon }) => {
   return (
     <div className={styles.footer_item_container}>
       <div className={styles.footer_item}><a href={href} target='_blank' rel='noreferrer'>{title}</a></div>
-      {Icon.map((Icon, index) => {
-        return (
-          <div key={index} className={styles.footer_item}>
-            <Icon href={href} styles={{ marginRight: '0', marginLeft: '0' }} />
-          </div>
-        )
-      })}
+      <div className={styles.icons}>
+        {Icon.map((Icon, index) => {
+          return (
+            <div key={index} className={styles.footer_item}>
+              <Icon href={href} styles={{ marginRight: '0', marginLeft: '0' }} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
