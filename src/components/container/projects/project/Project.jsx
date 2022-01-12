4554379f74e49e31fbe0cb4d85b24162fa2daace
EnzoLy <enzoloyola_@outlook.com>
2022-01-12 01:56:42 -0300

    adding clickable project card

--- src/components/container/projects/project/Project.jsx
+++ src/components/container/projects/project/Project.jsx
@@ -1,12 +1,10 @@
 import styles from './styles.module.scss'
 import { GitHubIcon, IconByName } from '../../../icons/Icons'
 
-export const Project = ({ title, url, teconologies }) => {
-  console.log(window.matchMedia('(min-width: 1288px)').matches)
-
+export const Project = ({ title, teconologies, url }) => {
   return (
     <div className={`${styles.project}`}>
-      <header>
+      <a href={url} target='_blank' rel='noreferrer'>
         <h2>{title}</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
@@ -14,18 +12,17 @@ export const Project = ({ title, url, teconologies }) => {
         <div>
           <div className={styles.item}><h3>Made with: </h3></div>
           {
-              teconologies.map((teconology, index) => {
-                return (
-                  <div className={styles.item} key={index}>
-                    <IconByName name={teconology} />
-                  </div>
-                )
-              })
-            }
+            teconologies.map((teconology, index) => {
+              return (
+                <div className={styles.item} key={index}>
+                  <IconByName name={teconology} />
+                </div>
+              )
+            })
+          }
         </div>
-        <a href='#' className={styles.visit}> Visit </a>
-      </header>
-      <footer><GitHubIcon href='https://github.com/enzoly' styles={{ fontSize: '2.5em' }} /></footer>
+      </a>
+      <footer><GitHubIcon href='https://github.com/enzoly' styles={{ fontSize: '2.5em', marginRight: '10px' }} /></footer>
     </div>
   )
 }
