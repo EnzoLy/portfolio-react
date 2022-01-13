623a487396152e924b6f0c78a58ca34b4e28bfae
EnzoLy <enzoloyola_@outlook.com>
2022-01-13 11:16:10 -0300

    Added description aboutme

--- src/components/container/aboutme/AboutMe.jsx
+++ src/components/container/aboutme/AboutMe.jsx
@@ -4,8 +4,15 @@ import avatarURL from '../../../assets/img/avatar.jpg'
 export const AboutMe = () => {
   return (
     <header className={style.aboutme} id='aboutme'>
-      <img src={avatarURL} alt='avatar' />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint odit officia nemo nulla molestiae inventore nihil delectus dolorum, recusandae nisi eligendi aspernatur eum illo amet asperiores, a laboriosam. Voluptas, numquam.</p>
+      <div>
+        <img src={avatarURL} alt='avatar' width='100px' className={style.aboutmeImage} />
+      </div>
+      <div className={style.aboutmeText}>
+        <span>My name is Enzo Loyola I currently live in Argentina. I am a Full-Stack developer.</span>
+        <span>My principal skills are: <span className={style.js}>JavaScript</span>, <span className={style.react}>React</span>, <span className={style.node}>NodeJs</span>, <span className={style.express}>ExpressJs</span>, <span className={style.mongodb}>MongoDB</span>, <span className={style.java}>Java</span> and <span className={style.kotlin}>Kotlin</span>.
+        </span>
+        <span>And now I'm current learning: <span className={style.nextjs}>NextJs</span>, <span className={style.firebase}>Firebase</span> and <span className={style.redux}>Redux</span>.</span>
+      </div>
     </header>
   )
 }
