1276a313e6aa0e3a6102aa7afbc1cca52e617f4c
EnzoLy <enzoloyola_@outlook.com>
2022-01-22 15:09:02 -0300

    better name for tabs

--- src/components/container/projects/tabs/Tabs.jsx
+++ src/components/container/projects/tabs/Tabs.jsx
@@ -4,9 +4,9 @@ import { Tab } from './tab/Tab'
 export const Tabs = ({ activeTab, handleTabClick }) => {
   return (
     <div className={styles.tabs}>
-      <Tab title='Real Projects' activeTab={activeTab} index={0} handleTabClick={handleTabClick} />
-      <Tab title='Minecraft Plugins' activeTab={activeTab} index={1} handleTabClick={handleTabClick} />
-      <Tab title='Personal Challenges' activeTab={activeTab} index={2} handleTabClick={handleTabClick} />
+      <Tab title='Web' activeTab={activeTab} index={0} handleTabClick={handleTabClick} />
+      <Tab title='Java' activeTab={activeTab} index={1} handleTabClick={handleTabClick} />
+      <Tab title='Personal' activeTab={activeTab} index={2} handleTabClick={handleTabClick} />
     </div>
   )
 }
