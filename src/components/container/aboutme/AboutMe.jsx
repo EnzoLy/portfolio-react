e66e71bd76d9b03e92086fd96d715e7aed639012
EnzoLy <enzoloyola_@outlook.com>
2022-01-22 15:07:37 -0300

    better aboutme (not finished)

--- src/components/container/aboutme/AboutMe.jsx
+++ src/components/container/aboutme/AboutMe.jsx
@@ -5,13 +5,42 @@ export const AboutMe = () => {
   return (
     <header className={style.aboutme} id='aboutme'>
       <div>
-        <img src={avatarURL} alt='avatar' width='100px' className={style.aboutmeImage} />
+        <img
+          src={avatarURL}
+          alt='avatar'
+          width='100px'
+          className={style.aboutmeImage}
+        />
       </div>
       <div className={style.aboutmeText}>
-        <span>My name is Enzo Loyola I currently live in Argentina. I am a Full-Stack developer.</span>
-        <span>My principal skills are: <span className={style.js}>JavaScript</span>, <span className={style.react}>React</span>, <span className={style.node}>NodeJs</span>, <span className={style.express}>ExpressJs</span>, <span className={style.mongodb}>MongoDB</span>, <span className={style.java}>Java</span> and <span className={style.kotlin}>Kotlin</span>.
+        <span>
+          My name is Enzo Loyola I am a Full-Stack developer from Argentina.
         </span>
-        <span>And now I'm current learning: <span className={style.nextjs}>NextJs</span>, <span className={style.firebase}>Firebase</span> and <span className={style.redux}>Redux</span>.</span>
+        <span>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+          reiciendis sequi natus
+        </span>
+        <br />
+        <div className={style.skills}>
+          <span>My principal skills are:</span>
+          <div>
+            <span className={style.js}>JavaScript</span>
+            <span className={style.react}>React</span>
+            <span className={style.node}>NodeJs</span>
+            <span className={style.express}>ExpressJs</span>
+            <span className={style.mongodb}>MongoDB</span>
+            <span className={style.java}>Java</span>
+            <span className={style.kotlin}>Kotlin</span>
+          </div>
+        </div>
+        <br />
+        <div className={style.skills}>
+          <span>And now I'm current learning:</span>
+          <div>
+            <span className={style.nextjs}>NextJs</span>
+            <span className={style.firebase}>Firebase</span>
+          </div>
+        </div>
       </div>
     </header>
   )
