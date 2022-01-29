7cedaaf10dc0d797b57360317d56961c83647ec9
EnzoLy <enzoloyola_@outlook.com>
2022-01-29 02:09:07 -0300

    feature: remove old styles

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,6 +1,5 @@
 import { Projects } from './projects/Projects'
 import { AboutMe } from './aboutme/AboutMe'
-import { Contact } from './contact/Contact'
 import styles from './styles.module.scss'
 
 export const Container = () => {
