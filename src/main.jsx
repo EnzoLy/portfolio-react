fd5bc284aa4c0a448ac602cd6be603a24020787f
EnzoLy <enzoloyola_@outlook.com>
2022-01-29 02:07:55 -0300

    feature: Adding react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Contact } from './components/container/contact/Contact.jsx'
 import { Container } from './components/container/Container.jsx'
 import { Footer } from './components/footer/Footer.jsx'
 import { Navbar } from './components/navbar/Navbar.jsx'
 import './styles.module.scss'
 
+const Home = () => {
+  return (
+    <>
+      <main>
+        <Container />
+      </main>
+    </>
+  )
+}
+
+const ContactPage = () => {
+  return (
+    <>
+      <main>
+        <Contact />
+      </main>
+    </>
+  )
+}
+
 const App = () => {
   return (
     <>
       <Navbar />
-      <main>
-        <Container />
-      </main>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
+      </div>
       <Footer />
     </>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'))
