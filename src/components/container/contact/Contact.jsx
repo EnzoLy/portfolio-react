ab052c935f0568fe630b70f215a944e28b59c9b1
EnzoLy <enzoloyola_@outlook.com>
2022-01-29 02:11:02 -0300

    feature: improvide code for better view

--- src/components/container/contact/Contact.jsx
+++ src/components/container/contact/Contact.jsx
@@ -2,85 +2,59 @@ import style from './styles.module.scss'
 import emailjs from 'emailjs-com'
 import { useState } from 'react'
 
-const EmailSendedComponent = () => {
-  return (
-    <div className={style.email_sended}>
-      <h3>
-        <span> Thank you for send mail! </span>
-      </h3>
-    </div>
-  )
-}
-
-const EmailSendingComponent = () => {
-  return (
-    <div className={style.email_sended}>
-      <h3>
-        <span> Sending... </span>
-      </h3>
-    </div>
-  )
-}
-
-const EmailErrorComponent = ({ error }) => {
-  return (
-    <div className={style.email_error}>
-      <h3>
-        <span> Error: {error} </span>
-        <span> Please contact with developer </span>
-      </h3>
-    </div>
-  )
+const EMAIL_STATE = {
+  SENDING: 'Thank you for send mail!',
+  SENDED: 'Sending...',
+  ERROR: 'Error, Please contact with developer',
+  NONE: 'NONE'
 }
 
 export const Contact = () => {
-  const [emailSend, setEmailSend] = useState(false)
-  const [emailError, setEmailError] = useState(null)
-  const [sending, setSending] = useState(false)
+  const [emailState, setEmailState] = useState(EMAIL_STATE.NONE)
 
   function onSubmit (event) {
     event.preventDefault()
 
-    setEmailError(null)
-    setSending(true)
+    setEmailState(EMAIL_STATE.SENDING)
 
     emailjs.sendForm(import.meta.env.VITE_SERVICE_ID_EMAILJS, import.meta.env.VITE_TEMPLATE_ID_EMAILJS, event.target, import.meta.env.VITE_USER_ID_EMAILJS)
       .then((result) => {
-        setSending(false)
-        setEmailSend(true)
         if (result.text === 'OK') {
+          setEmailState(EMAIL_STATE.SENDED)
           setTimeout(() => {
-            setEmailSend(false)
+            setEmailState(EMAIL_STATE.NONE)
           }, 4000)
         }
       }, (error) => {
-        setSending(false)
-        setEmailSend(error.text)
+        console.log(error.text)
+        setEmailState(EMAIL_STATE.ERROR)
       })
   }
 
   return (
     <section id='contact' className={style.contact_container} onSubmit={onSubmit}>
       {
-        emailError ? <EmailErrorComponent error={emailError} /> : null
+        emailState !== EMAIL_STATE.NONE && (
+          <div className={style.email_sended}>
+            <h3>
+              <span> {emailState} </span>
+            </h3>
+          </div>
+        )
       }
       {
-        sending
-          ? <EmailSendingComponent />
-          : emailSend
-            ? <EmailSendedComponent />
-            : (
-              <div>
-                <h2>CONTACT</h2>
-                <form className={style.contact}>
-                  <span>Email </span>
-                  <input type='email' name='email' placeholder='Email' required />
-                  <span>Message </span>
-                  <textarea name='message' placeholder='Message' required />
-                  <button type='submit'>Send</button>
-                </form>
-              </div>
-              )
+        emailState === EMAIL_STATE.NONE && (
+          <div>
+            <h2>CONTACT</h2>
+            <form className={style.contact}>
+              <span>Email </span>
+              <input type='email' name='email' placeholder='Email' required />
+              <span>Message </span>
+              <textarea name='message' placeholder='Message' required />
+              <button type='submit'>Send</button>
+            </form>
+          </div>
+        )
       }
     </section>
   )
