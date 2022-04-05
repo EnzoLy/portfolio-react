821740b3b6a861f41cfc6fa278ca81d9628b011f
EnzoLy <enzoloyola_@outlook.com>
2022-04-04 22:39:16 -0300

    feature: create const of projects

--- src/services/getAllProjects.js
+++ src/services/getAllProjects.js
@@ -1,108 +1,106 @@
-export const getAllProjects = (tabIndex) => {
-  const projects = [
-    {
-      tab: 0,
-      title: 'Project 1',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: true,
-      teconologies: ['React', 'Vite']
-    }, {
-      tab: 0,
-      title: 'Project 2',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['React', 'Vite']
-    }, {
-      tab: 0,
-      title: 'Project 3',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['NextJs']
-    }, {
-      tab: 0,
-      title: 'Project 4',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['NextJs']
-    },
-    {
-      tab: 1,
-      title: 'Project 5',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: true,
-      teconologies: ['Java']
-    }, {
-      tab: 1,
-      title: 'Project 6',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['Java']
-    }, {
-      tab: 1,
-      title: 'Project 7',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['Kotlin']
-    }, {
-      tab: 1,
-      title: 'Project 8',
-      url: 'https://github.com/enzoly',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'lore ipsum',
-      recent: false,
-      teconologies: ['Kotlin']
-    },
-    {
-      tab: 2,
-      title: 'MichiClone',
-      url: 'https://instagram.com',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'A clone of Instagram but i upload images and videos of my cats',
-      recent: true,
-      teconologies: ['NextJs', 'Firebase']
-    },
-    {
-      tab: 2,
-      title: 'MichiClone',
-      url: 'https://instagram.com',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'A clone of Instagram but i upload images and videos of my cats',
-      recent: true,
-      teconologies: ['NextJs', 'Firebase']
-    },
-    {
-      tab: 2,
-      title: 'MichiClone',
-      url: 'https://instagram.com',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'A clone of Instagram but i upload images and videos of my cats',
-      recent: true,
-      teconologies: ['NextJs', 'Firebase']
-    },
-    {
-      tab: 2,
-      title: 'MichiClone',
-      url: 'https://instagram.com',
-      imgUrl: 'https://via.placeholder.com/300x200',
-      description: 'A clone of Instagram but i upload images and videos of my cats',
-      recent: true,
-      teconologies: ['NextJs', 'Firebase']
-    }
-  ]
+const projects = [
+  {
+    tab: 0,
+    title: 'Project 1',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: true,
+    teconologies: ['React', 'Vite']
+  }, {
+    tab: 0,
+    title: 'Project 2',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['React', 'Vite']
+  }, {
+    tab: 0,
+    title: 'Project 3',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['NextJs']
+  }, {
+    tab: 0,
+    title: 'Project 4',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['NextJs']
+  },
+  {
+    tab: 1,
+    title: 'Project 5',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: true,
+    teconologies: ['Java']
+  }, {
+    tab: 1,
+    title: 'Project 6',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['Java']
+  }, {
+    tab: 1,
+    title: 'Project 7',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['Kotlin']
+  }, {
+    tab: 1,
+    title: 'Project 8',
+    url: 'https://github.com/enzoly',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'lore ipsum',
+    recent: false,
+    teconologies: ['Kotlin']
+  },
+  {
+    tab: 2,
+    title: 'MichiClone',
+    url: 'https://instagram.com',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'A clone of Instagram but i upload images and videos of my cats',
+    recent: true,
+    teconologies: ['NextJs', 'Firebase']
+  },
+  {
+    tab: 2,
+    title: 'MichiClone',
+    url: 'https://instagram.com',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'A clone of Instagram but i upload images and videos of my cats',
+    recent: true,
+    teconologies: ['NextJs', 'Firebase']
+  },
+  {
+    tab: 2,
+    title: 'MichiClone',
+    url: 'https://instagram.com',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'A clone of Instagram but i upload images and videos of my cats',
+    recent: true,
+    teconologies: ['NextJs', 'Firebase']
+  },
+  {
+    tab: 2,
+    title: 'MichiClone',
+    url: 'https://instagram.com',
+    imgUrl: 'https://via.placeholder.com/300x200',
+    description: 'A clone of Instagram but i upload images and videos of my cats',
+    recent: true,
+    teconologies: ['NextJs', 'Firebase']
+  }
+]
 
-  return Promise.resolve(projects.filter(project => project.tab === tabIndex))
-}
+export const getAllProjects = (tabIndex) => Promise.resolve(projects.filter(project => project.tab === tabIndex))
